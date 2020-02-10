9138fb003f198a7e1a3c61797b24b7487d0d308c
TobiasFalleth <60098027+TobiasFalleth@users.noreply.github.com>
2020-02-10 13:55:35 +0100

    Lagt til knapp og fikset litt funksjonalitet

--- APP2000/TipsForTripsDesktop/MainWindow.xaml.cs
+++ APP2000/TipsForTripsDesktop/MainWindow.xaml.cs
@@ -20,14 +20,44 @@ namespace TipsForTripsDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        string username = "Test";
+        string password = "Test123";
+
+        string sqlUsername = "Test";
+        string sqlPassword = "Test123";
+
         public MainWindow()
         {
             InitializeComponent();
+            Test();
         }
 
-        public void Slider()
+        public void Test()
         {
-            
+            MessageBox.Show("Log in");
+            if (username.Equals(getUsername(sqlUsername)) && password.Equals(getPassword(sqlPassword)))
+            {
+                MessageBox.Show("Hei, Randi!");
+            }
+            else
+            {
+                MessageBox.Show("ERROR#3144 - Wrong username or password. Please try again!");
+            }
+        }
+
+        private string getUsername(string checkUsername)
+        {
+            return checkUsername;
+        }
+
+        private String getPassword(string checkPassword)
+        {
+            return checkPassword;
+        }
+
+        void OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hei, Randi!");
         }
     }
 }
