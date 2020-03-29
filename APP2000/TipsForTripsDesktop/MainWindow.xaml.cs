345a1e2b2e0163d657ce3b915fb2697ff1715b2b
Raneik <randi_heggset@hotmai.com>
2020-03-29 14:44:37 +0200

    Updated delete-button in basic info

--- APP2000/TipsForTripsDesktop/MainWindow.xaml.cs
+++ APP2000/TipsForTripsDesktop/MainWindow.xaml.cs
@@ -396,7 +396,7 @@ namespace TipsForTripsDesktop
             }
             else if (b.Equals(Basic_info_Button))
             {
-                Content_Frame.Content = new Basic_info(user);
+                Content_Frame.Content = new Basic_info(user, this);
             }
             else if (b.Equals(Password_Button))
             {
--- APP2000/TipsForTripsDesktop/Password.xaml.cs
+++ APP2000/TipsForTripsDesktop/Password.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -63,6 +64,34 @@ namespace TipsForTripsDesktop
                 Success.DataContext = "Password updated successfully!";
             }
             
+        }
+
+        // Enter animation
+        private void Button_Enter(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            ColorAnimation buttonAnimation = new ColorAnimation();
+            ColorAnimation textAnimation = new ColorAnimation();
+
+            buttonAnimation.From = (Color)ColorConverter.ConvertFromString("#4296d6");
+            buttonAnimation.To = (Color)ColorConverter.ConvertFromString("#6caddf");
+            buttonAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6caddf"));
+            b.Background.BeginAnimation(SolidColorBrush.ColorProperty, buttonAnimation);
+        }
+
+        // Leave animation
+        private void Button_Leave(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            ColorAnimation buttonAnimation = new ColorAnimation();
+            ColorAnimation textAnimation = new ColorAnimation();
+
+            buttonAnimation.From = (Color)ColorConverter.ConvertFromString("#6caddf");
+            buttonAnimation.To = (Color)ColorConverter.ConvertFromString("#4296d6");
+            buttonAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4296d6"));
+            b.Background.BeginAnimation(SolidColorBrush.ColorProperty, buttonAnimation);
         }
 
         // Database connection
