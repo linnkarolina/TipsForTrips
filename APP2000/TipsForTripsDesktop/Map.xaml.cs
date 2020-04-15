023bc8d06cd9c6d8ce1e1cf3ae3bdfa903b604bf
TobiasFalleth <60098027+TobiasFalleth@users.noreply.github.com>
2020-04-15 14:27:37 +0200

    Added content to Map

--- APP2000/TipsForTripsDesktop/Map.xaml.cs
+++ APP2000/TipsForTripsDesktop/Map.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,38 @@ namespace TipsForTripsDesktop
         public Map()
         {
             InitializeComponent();
+        }
+
+        private void Button_Enter(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            ColorAnimation buttonAnimation = new ColorAnimation();
+            ColorAnimation textAnimation = new ColorAnimation();
+
+            buttonAnimation.From = (Color)ColorConverter.ConvertFromString("#4296d6");
+            buttonAnimation.To = (Color)ColorConverter.ConvertFromString("#6caddf");
+            buttonAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6caddf"));
+            b.Background.BeginAnimation(SolidColorBrush.ColorProperty, buttonAnimation);
+        }
+
+        // Leave animation
+        private void Button_Leave(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            ColorAnimation buttonAnimation = new ColorAnimation();
+            ColorAnimation textAnimation = new ColorAnimation();
+
+            buttonAnimation.From = (Color)ColorConverter.ConvertFromString("#6caddf");
+            buttonAnimation.To = (Color)ColorConverter.ConvertFromString("#4296d6");
+            buttonAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4296d6"));
+            b.Background.BeginAnimation(SolidColorBrush.ColorProperty, buttonAnimation);
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("RANDI ER BEST!");
         }
     }
 }
